bf99a090a06ff0f35d3d5c2d8da2396ea96ad1c7
Robert Gitonga 150233 <128735598+bobgitonga@users.noreply.github.com>
2023-11-18 08:45:10 +0300

    Create constants.php

---
+++ constants.php
@@ -0,0 +1,8 @@
+<?php
+// constants.php
+
+define('DB_HOST', 'your_database_host');
+define('DB_NAME', 'authordb');
+define('DB_USER', 'your_database_user');
+define('DB_PASSWORD', 'your_database_password');
+?>
