34bbacc1b65a8018f3ce07fd3f8de797bfc28fcc
Robert Gitonga 150233 <128735598+bobgitonga@users.noreply.github.com>
2023-11-18 09:22:26 +0300

    Create authorsreg.php

---
+++ authorsreg.php
@@ -0,0 +1,8 @@
+<?php
+// AutRegistration.php
+
+require_once('../configs/DbConn.php');
+
+// Retrieve data from the form submission and insert into the database
+// Use prepared statements to prevent SQL injection
+?>
