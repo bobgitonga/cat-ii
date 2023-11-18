88e591b3ec8dd16b507ae622b0be62aa949ce606
Robert Gitonga 150233 <128735598+bobgitonga@users.noreply.github.com>
2023-11-18 09:22:56 +0300

    Create viewauthors.php

---
+++ viewauthors.php
@@ -0,0 +1,9 @@
+<?php
+// ViewAuthors.php
+
+require_once('../configs/DbConn.php');
+
+// Fetch all authors from the database and display in a table sorted by AuthorFullName
+// Use Bootstrap table classes for styling
+?>
+
