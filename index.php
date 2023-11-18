d72b289bbc33424431d8c8d829243cf30ebb2885
Robert Gitonga 150233 <128735598+bobgitonga@users.noreply.github.com>
2023-11-18 08:33:05 +0300

    Create index.php

---
+++ index.php
@@ -0,0 +1,16 @@
+<!-- index.php -->
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Author Registration Form</title>
+</head>
+<body>
+    <form action="processes/AuthRegistration.php" method="post">
+        <!-- Include necessary input fields for Author details -->
+        <!-- Use appropriate input types for each field -->
+        <!-- Make sure to set proper name attributes for form submission -->
+    </form>
+</body>
+</html>
